f9008ed0a25a85b0c8a7811e0a7164e7772ffa18
Irmak Erdem <irmakerdem9@gmail.com>
2022-02-20 10:39:22 -0700

    Refactor all 3 files

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes
 var mains = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesan', 'Butternut Squash Soup', 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza'];
 var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macarons', 'Macaroons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs'];
 
-//create global variables
 var letsCookButton = document.querySelector(".cook-button");
 var mealTitleText = document.querySelector(".meal-title-text");
 var randomMealText = document.querySelector(".random-meal-text");
@@ -13,12 +12,10 @@ var entireMealOption = document.querySelector("#entireMeal");
 function getRandomElement(array) {
   return array[Math.floor(Math.random()*array.length)]
 }
-//outputs value of the index
 
 function getRecipe() {
   var grabRadio = document.querySelector('input[name="meal-options"]:checked');
   if (!grabRadio) {
-    // console.log("truthy");
     return
   }
     if (grabRadio.value === "sidesV") {
@@ -55,5 +52,3 @@ function displayMeal() {
   show(clearButton);
   randomMealText.innerText = getRecipe();
 }
-
-//feature/create-readme
