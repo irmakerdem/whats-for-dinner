24aa1d3c5b028268b7fcb4b90dc63c80b956798a
Irmak Erdem <irmakerdem9@gmail.com>
2022-02-20 10:27:02 -0700

    Add entire meal functionality

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var mealTitleText = document.querySelector(".meal-title-text");
 var randomMealText = document.querySelector(".random-meal-text");
 var potImage = document.querySelector(".pot-img");
 var clearButton = document.querySelector(".clear-button");
+var entireMealOption = document.querySelector("#entireMeal");
 
 function getRandomElement(array) {
   return array[Math.floor(Math.random()*array.length)]
@@ -29,11 +30,14 @@ function getRecipe() {
     else if (grabRadio.value === "dessertsV") {
       return `${getRandomElement(desserts)}!`
     }
-  displayMeal();
+    else if (grabRadio.value === "entireV") {
+      return `${getRandomElement(mains)} with a side of ${getRandomElement(desserts)} and ${getRandomElement(desserts)} for dessert!`
+    }
+    displayMeal();
 }
 
 letsCookButton.addEventListener('click', displayMeal);
-
+entireMealRadio.addEventListener('click', showAllMeals);
 
 function show(element) {
   element.classList.remove('hidden');
@@ -51,14 +55,5 @@ function displayMeal() {
   show(clearButton);
   randomMealText.innerText = getRecipe();
 }
-
-//feature/select-entire-meal-option
-//select Entire Meal radio button
-//click cook-button
-//hide pot-img
-//show string ‘You should make: ${} with a side of ${} and ${} for dessert!’
-  //choose random 3 dishes from array (one of each kind)
-//show clear-button
-
 
 //feature/create-readme
